13a23b7294a8ca2fcd61ed2ef52b943604c2e14c
zi-seong <jiseong4577@gmail.com>
2024-06-16 14:27:19 +0900

    create project

---
+++ src/main/java/com/zi_seong/jmz/JmzApplication.java
@@ -0,0 +1,13 @@
+package com.zi_seong.jmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zi_seong/jmz/JmzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zi_seong.jmz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
